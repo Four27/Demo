33b9469560395bf69e57ab84c1456454f49abf16
Shirley <1733025299@qq.com>
2017-08-07 14:17:33 +0800

    add file upload & text upload

--- nodeDemo/route.js
+++ nodeDemo/route.js
@@ -1,10 +1,15 @@
-function route(handle, pathname) {
+function route(handle, pathname, response, request) {
     console.log('About to route a request for:' + pathname);
 
-    if(typeof handle[pathname] === 'function') {
-        handle[pathname]();               //不是很明白这里为什么要加();
+    if (typeof handle[pathname] === 'function') {
+        // return handle[pathname](response, postData);    文本请求    //不是很明白这里为什么要加()：因为handle[pathname]目前只是代表start或者upload;
+        return handle[pathname](response, request);
     } else {
         console.log('No request handler found for ' + pathname);
+
+        response.writeHead(404, { 'Content-Type': 'text/plain' });
+        response.write('404 Not found');
+        response.end();
     }
 }
 
